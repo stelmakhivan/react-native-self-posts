44ed193d165a562ef28787543bac5eac0dba49a3
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-02-04 13:59:04 +0200

    Added expo-font

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { AppLoading } from 'expo'
+
+import { bootstrap } from './src/bootstrap'
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false)
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={bootstrap}
+        onFinish={() => setIsReady(true)}
+        onError={console.log}
+      />
+    )
+  }
+
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
