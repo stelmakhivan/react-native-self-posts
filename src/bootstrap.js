9163e3e56ca38613a539f4297dc41bd99048a825
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-02-08 14:57:33 +0200

    initialise SQLite db

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,8 +1,15 @@
 import * as Font from 'expo-font'
+import { DB } from './db'
 
 export async function bootstrap() {
-  await Font.loadAsync({
-    'open-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
-    'open-regular': require('../assets/fonts/OpenSans-Regular.ttf')
-  })
+  try {
+    await Font.loadAsync({
+      'open-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+      'open-regular': require('../assets/fonts/OpenSans-Regular.ttf'),
+    })
+    await DB.init()
+    console.log('Database initialized...')
+  } catch (error) {
+    console.log('bootstrap error', error)
+  }
 }
